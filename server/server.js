ad1146a0b768c98fe5924fd37d1e02a426fdfe67
Sami Dalati <sami.aldalati@gmail.com>
2018-04-22 12:34:51 -0400

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express     = require('express');
+var bodyParser  = require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/users');
+var {mongoose}  = require('./db/mongoose');
+var {Todo}      = require('./models/todo');
+var {User}      = require('./models/users');
+
+const {ObjectID}    =   require('mongodb');
+
 
 const port = 3000;
 
@@ -37,6 +40,33 @@ app.get('/todos', (req, res) => {
         res.status(400).send(err);
     });
 })
+
+app.get('/todos/:id', (req, res) => {
+    // res.send(req.params);
+
+    //validate id
+    if(!ObjectID.isValid(req.params.id)){
+        console.log("Invaild ID")
+        // if not vaild then 404 and empty body
+        return res.status(404).send();
+    }
+    
+    Todo.findById(req.params.id).then((todo) => {
+        if(!todo){
+            // if no todo, send back 404 with empty body
+            console.log("ID not found!");
+            return res.status(404).send();
+        }
+        //sucess
+        console.log('Todo by id: ', todo);
+        // if todo, send it back
+        res.send({todo});
+    }).catch((e) => {
+        //400 - and send empty body back
+        console.log(e);
+        res.status(400).send();
+    })
+});
 
 app.listen(port, () => {
 
